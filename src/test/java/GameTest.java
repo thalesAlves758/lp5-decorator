bcb20c8a8eb13693b9d7228daf6554a1ee675869
thalesAlves758 <thalesalves758@gmail.com>
2023-04-01 16:14:57 -0300

    test: create should return base game value test

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameTest {
+    @Test
+    void shouldReturnBaseGameValue() {
+        Game game = new BaseGame(300f);
+
+        assertEquals(300f, game.getGameValue());
+    }
+}
