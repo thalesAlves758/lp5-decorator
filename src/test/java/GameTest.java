5fa5614e53107bc039cbfe2a248585083e3c7e2c
thalesAlves758 <thalesalves758@gmail.com>
2023-04-01 16:16:30 -0300

    test: create should return base game with season pass value test

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -9,4 +9,11 @@ public class GameTest {
 
         assertEquals(300f, game.getGameValue());
     }
+
+    @Test
+    void shouldReturnBaseGameWithSeasonPassValue() {
+        Game game = new SeasonPass(new BaseGame(300f));
+
+        assertEquals(360f, game.getGameValue());
+    }
 }
